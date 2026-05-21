76fd3a6a2ac363b7f83fbb346fd300552366f876
Hrijul Bhatnagar <aviusanima@gmail.com>
2026-05-21 13:14:23 +0530

    refactor: normalize recipe ingredients database schema and strictly type Go models

--- backend/models/recipe.go
+++ backend/models/recipe.go
@@ -1,9 +1,15 @@
 package models
 
+type Ingredient struct {
+	Name   string `json:"name"`
+	Weight int    `json:"weight"`
+	Unit   string `json:"unit"`
+}
+
 type Recipe struct {
-	ID          int         `json:"id"`
-	Title       string      `json:"title"`
-	Image       *string     `json:"image"`
-	PrepTime    int         `json:"prep_time"`
-	Ingredients interface{} `json:"ingredients"`
+	ID          int          `json:"id"`
+	Title       string       `json:"title"`
+	Image       *string      `json:"image"`
+	PrepTime    int          `json:"prep_time"`
+	Ingredients []Ingredient `json:"ingredients"`
 }
